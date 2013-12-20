4855c06e6a389ef191e9102dc7e5c716daffa964
raaar <chrishanham@Dave--Sams-iMac.local>
2013-12-20 12:02:08 +0000

    Added IE8 Stylesheet

--- lib/enqueue-style.php
+++ lib/enqueue-style.php
@@ -7,13 +7,22 @@ function reverie_enqueue_style()
 {
 
 	// foundation stylesheet
-	wp_register_style( 'reverie-foundation-stylesheet', get_template_directory_uri() . '/css/app.css', array(), '' );
+	//wp_register_style( 'reverie-foundation-stylesheet', get_template_directory_uri() . '/css/app.css', array(), '' );
 
 	// Register the main style
-	wp_register_style( 'reverie-stylesheet', get_template_directory_uri() . '/css/style.css', array(), '', 'all' );
+	wp_register_style( 'stylesheet', get_template_directory_uri() . '/css/style.css', array(), '', 'all' );
+    
+    global $is_IE;
+    if ($is_IE) {
+		wp_register_style( 'style-ie8', get_template_directory_uri() . '/css/style-ie8.css', array(), '', 'all' );
+    }
 
-	wp_enqueue_style( 'reverie-foundation-stylesheet' );
-	wp_enqueue_style( 'reverie-stylesheet' );
+
+//	wp_enqueue_style( 'reverie-foundation-stylesheet' );
+	wp_enqueue_style( 'stylesheet' );
+	wp_enqueue_style( 'style-ie8' );
+
+
 }
 add_action( 'wp_enqueue_scripts', 'reverie_enqueue_style' );
 ?>
